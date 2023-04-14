1e390141540bbd679d6940baeb97c443dbf9c014
William <yydswuweilin@outlook.com>
2023-04-14 23:22:21 +0800

    first commit

---
+++ src/main/java/com/wwl/GitTest.java
@@ -0,0 +1,11 @@
+package com.wwl;
+
+/**
+ * @author WWL
+ * @date 2023/4/14 23:18
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("wwl好帅");
+    }
+}
