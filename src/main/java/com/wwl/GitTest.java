a2e2c6d062ce28977481f0da7fc74e9835b6fade
William <yydswuweilin@outlook.com>
2023-04-15 10:53:25 +0800

    first commit

--- src/main/java/com/wwl/GitTest.java
+++ src/main/java/com/wwl/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("wwl好帅");
         System.out.println("wwl好真的帅");
+        System.out.println("帅");
     }
 }
